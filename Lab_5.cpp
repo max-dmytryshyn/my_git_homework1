a4fad9344daf908b8893f2825e31de950fe197ef
max-dmytryshyn <maksumus22@gmail.com>
2020-12-12 16:00:25 +0200

    Rewriting code in Lab_5.cpp into more readable and informative version

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -2,112 +2,119 @@
 #include "vector"
 #include "iomanip"
 using Matrix = std::vector<std::vector<int>>;
-std::istream& operator>>(std::istream& is, Matrix& m) {
-	int rows = m.size();
-	int columns = m[0].size();
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			is >> m[i][j];
-		}
-	}
-	return is;
-}
-std::ostream& operator<<(std::ostream& os, Matrix m) {
-	int rows = m.size();
-	int columns = m[0].size();
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			os << std::setw(3) << m[i][j];
 
-			os << ' ';
-		}
-		os << '\n';
-	}
-	return os;
+std::ostream& operator<<(std::ostream& os, std::vector<int>& vec) {
+    int size = vec.size();
+    for (int i = 0; i < size; i++) {
+            os << std::setw(3) << vec[i];
+            os << ' ';
+    }
+    os << '\n';
+    return os;
 }
-std::ostream& operator<<(std::ostream& os, std::vector<int> v) {
-	int size = v.size();
-	for (int i = 0; i < size; i++) {
-			os << std::setw(3) << v[i];
-			os << ' ';
-	}
-	os << '\n';
-	return os;
+
+std::istream& operator>>(std::istream& is, Matrix& matrix) {
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            is >> matrix[i][j];
+        }
+    }
+    return is;
 }
-void merge(std::vector<int>& vec, int start, int middle, int end) {
-	int n1 = middle - start + 1;
-	int n2 = end - middle;
-	std::vector<int> a(n1);
-	std::vector<int> b(n2);
-	for (int i = 0; i < n1; i++) {
-		a[i] = vec[start + i];
-	}
-	for (int i = 0; i < n2; i++) {
-		b[i] = vec[i + middle + 1];
-	}
-	int k = 0, m = 0;
-	for (int i = start; i <= end; i++) {
-		if (k >= n1) {
-			vec[i] = b[m];
-			m++;
-			continue;
-		}
-		if(m >= n2){
-			vec[i] = a[k];
-			k++;
-			continue;
-		}
-		if (a[k] <= b[m]) {
-			vec[i] = a[k];
-			k++;
-		}
-		else {
-			vec[i] = b[m];
-			m++;
-		}
-	}
+
+std::ostream& operator<<(std::ostream& os, Matrix& matrix) {
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    for (int i = 0; i < rows; i++) {
+        os << matrix[i];
+    }
+    return os;
 }
-void merge_sort(std::vector<int>& vec, int start, int end) {
-	if (start < end) {
-		int m = start + (end - start) / 2;
-		merge_sort(vec, start, m);
-		merge_sort(vec, m + 1, end);
-		merge(vec, start, m, end);
-	}
+
+void merge_halves_of_vector(std::vector<int>& vec, int start_of_vector, int middle_of_vector, int end_of_vector) {
+    int first_half_of_vector_size = middle_of_vector - start_of_vector + 1;
+    int second_half_of_vector_size = end_of_vector - middle_of_vector;
+    std::vector<int> first_half_of_vector(first_half_of_vector_size);
+    std::vector<int> second_half_of_vector(second_half_of_vector_size);
+    for (int i = 0; i < first_half_of_vector_size; i++) {
+        first_half_of_vector[i] = vec[start_of_vector + i];
+    }
+    for (int i = 0; i < second_half_of_vector_size; i++) {
+        second_half_of_vector[i] = vec[i + middle_of_vector + 1];
+    }
+    int index_of_element_in_first_half = 0,
+    index_of_element_in_second_half = 0;
+    for (int i = start_of_vector; i <= end_of_vector; i++) {
+        if (index_of_element_in_first_half >= first_half_of_vector_size) {
+            vec[i] = second_half_of_vector[index_of_element_in_second_half];
+            index_of_element_in_second_half++;
+            continue;
+        }
+        if(index_of_element_in_second_half >= second_half_of_vector_size){
+            vec[i] = first_half_of_vector[index_of_element_in_first_half];
+            index_of_element_in_first_half++;
+            continue;
+        }
+        if (first_half_of_vector[index_of_element_in_first_half] <= second_half_of_vector[index_of_element_in_second_half]) {
+            vec[i] = first_half_of_vector[index_of_element_in_first_half];
+            index_of_element_in_first_half++;
+        }
+        else {
+            vec[i] = second_half_of_vector[index_of_element_in_second_half];
+            index_of_element_in_second_half++;
+        }
+    }
 }
-std::vector<int> f(Matrix m) {
-	int size = m.size();
-	std::vector<int> result(size);
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i - 1; j++) {
-			result[i] += m[j][i];
-		}
-	}
-	return result;
+
+void merge_sort_vector(std::vector<int>& vec, int start_of_vector, int end_of_vector) {
+    if (start_of_vector < end_of_vector) {
+        int middle_of_vector = start_of_vector + (end_of_vector - start_of_vector) / 2;
+        merge_sort_vector(vec, start_of_vector, middle_of_vector);
+        merge_sort_vector(vec, middle_of_vector + 1, end_of_vector);
+        merge_halves_of_vector(vec, start_of_vector, middle_of_vector, end_of_vector);
+    }
 }
-double F(std::vector<int> vec) {
-	double product = 1;
-	int size = vec.size() - 1;
-	for (int i = 0; i < size; i++) {
-		product = product*vec[i];
-		if (vec[i] == 0)
-			return 0;
-	}
-	if (product < 0) {
-		return 0;
-	}
-	return pow(product, 1 / (double)size);
+
+void merge_sort_matrix_in_rows(Matrix& matrix){
+    int size = matrix.size();
+    for (int i = 0; i < size; i++) {
+        merge_sort_vector(matrix[i], 0, size - 1);
+    }
+}
+
+std::vector<int> calculate_sums_of_elements_over_side_diagonal_in_rows(Matrix matrix) {
+    int size = matrix.size();
+    std::vector<int> result(size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            result[i] += matrix[j][i];
+        }
+    }
+    return result;
+}
+double geometric_mean_of_vector_elements(std::vector<int> vec) {
+    double product = 1;
+    int size = vec.size() - 1;
+    for (int i = 0; i < size; i++) {
+        product = product*vec[i];
+        if (vec[i] == 0)
+            return 0;
+    }
+    if (product < 0) {
+        return 0;
+    }
+    return pow(product, 1 / (double)size);
 }
 int main() {
-	Matrix matrix(5, std::vector<int>(5));
-	std::cin >> matrix;
-	std::cout << '\n' << matrix << '\n';
-	int size = matrix.size();
-	for (int i = 0; i < size; i++) {
-		merge_sort(matrix[i], 0, size - 1);
-	}
-	std::cout << matrix << '\n';
-	std::cout << f(matrix) << '\n';
-	std::cout << F(f(matrix));
-	return 0;
+    Matrix matrix(5, std::vector<int>(5));
+    std::cin >> matrix;
+    std::cout << '\n' << matrix << '\n';
+    merge_sort_matrix_in_rows(matrix);
+    std::cout << matrix << '\n';
+    std::vector<int> sums_of_elements_over_side_diagonal = calculate_sums_of_elements_over_side_diagonal_in_rows(matrix);
+    std::cout << sums_of_elements_over_side_diagonal << '\n';
+    std::cout << geometric_mean_of_vector_elements(sums_of_elements_over_side_diagonal) << '\n';
+    return 0;
 }
